9f3eb4ff4ebb70d574b4d896655f4cfd597608bf
alinemelry <aline.melryaa@gmail.com>
2022-01-14 22:21:52 -0300

    useParams

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,7 +1,7 @@
 import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
 import { Movie } from "types/movie";
-import './styles.css';
+
 
 
 
@@ -9,19 +9,19 @@ type Props = {
     movie: Movie;
 }
 
-function MovieCard({ movie } : Props) {
+function MovieCard({ movie }: Props) {
 
     return(
-    <div>
-        <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-        <div className="dsmovie-card-bottom-container">
-        <h3>{movie.title}</h3>
-        <MovieScore />
-        <Link to={`/form/${movie.id}`}>
-        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
-        </Link>
-    </div>
-</div>
+        <div>
+         <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+         <div className="dsmovie-card-bottom-container">
+             <h3>{movie.title}</h3>
+                <MovieScore count={movie.count} score={movie.score}/>
+             <Link to={`/form/${movie.id}`}>
+                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+             </Link>
+         </div>
+        </div>
     );
 }
 
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -18,28 +18,33 @@ function Listing(){
             number: 0,
             first: true,
             numberOfElements: 0,
-            empty: true
+            empty: true,
         }
     );
     useEffect(() =>{
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
         .then(response => {
         const data = response.data as MoviePage;
         setPage(data);
     });
-    }, [pageNumber]);   
+    }, [pageNumber]); 
+    
+    const handlePageChange = (newPageNumber: number) => {
+    setPageNumber(newPageNumber);
+
+    }
 
     return (
         <>
         <Pagination />
         <div className="container">
             <div className="row">
-                {page.content.map(movie =>(
-                <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                    <MovieCard movie={movie}/>
-                </div>  
-                    )
-                 )}        
+            {page.content.map(movie => (
+            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard movie={movie} />
+            </div>
+          )
+          )}       
     
             </div>
         </div>
