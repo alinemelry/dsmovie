8e28c4584ce1716290265e49b8b994b5fa4b2500
alinemelry <aline.melryaa@gmail.com>
2022-01-15 15:19:43 -0300

    correcao no aquivo movieControlle-esquecie de envolver o id entre chaves

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -4,9 +4,9 @@ import { useParams } from 'react-router-dom';
 function Form() {
    const params = useParams();
 
-    return (
-        <FormCard movieId={`${params.movieId}`} />
-    );       
+   return (
+    <FormCard movieId={`${params.movieId}`}/>
+);      
 }
 
 export default Form;
